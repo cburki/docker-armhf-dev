3b5517ef4625b1390775616688ff1d88d7973842
Christophe Burki <christophe.burki@gmail.com>
2015-10-14 12:37:50 +0200

    Added bash prompt

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,6 +2,7 @@
 
 STATUS_FILE=/opt/rpi-toolchain.status
 TOOLCHAIN_PATH=/opt/rpi-toolchain
+LIB_PATH=/opt/usr/
 
 if [ -f ${STATUS_FILE} ]; then
 
@@ -11,9 +12,11 @@ fi
 
 echo "Installing the toolchain"
 mkdir -p ${TOOLCHAIN_PATH}
+mkdir -p ${LIB_PATH}
 cd ${TOOLCHAIN_PATH}
 git clone git://github.com/raspberrypi/tools.git
 
+echo 'PS1="\[\e[00;36m\][\$?]\[\e[0m\]\[\e[00;30m\] \[\e[0m\]\[\e[00;32m\]\u@\h\[\e[0m\]\[\e[00;30m\] \[\e[0m\]\[\e[00;34m\][\W]\[\e[0m\]\[\e[00;30m\] \\$ \[\e[0m\]"' >> /root/.bashrc
 echo "PATH=${PATH}:${TOOLCHAIN_PATH}/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/bin" >> /root/.bashrc
 
 echo "done" > ${STATUS_FILE}
