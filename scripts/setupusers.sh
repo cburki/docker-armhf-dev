df16ac52817400513711907af317857a3a0a9633
Christophe Burki <christophe.burki@gmail.com>
2016-03-23 14:31:38 +0100

    Do not start tmux automatically

--- scripts/setupusers.sh
+++ scripts/setupusers.sh
@@ -47,7 +47,7 @@ if [ -n "${SSH_USER}" ]; then
         echo "TERM=xterm" >> /home/${SSH_USER}/.bashrc
         echo 'PS1="\[\e[32m\]\u\[\e[m\]\[\e[32m\]@\[\e[m\]\[\e[32m\]\h\[\e[m\]\[\e[32m\]:\[\e[m\]\[\e[34m\]\W\[\e[m\] \[\e[34m\]\\$\[\e[m\] "' >> /home/${SSH_USER}/.bashrc
         echo "PATH=${PATH}:${TOOLCHAIN_PATH}/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/bin" >> /home/${SSH_USER}/.bashrc
-        echo '[ -z "$TMUX" ] && command -v tmux > /dev/null && tmux && exit 0' >> /home/${SSH_USER}/.bashrc
+        echo '#[ -z "$TMUX" ] && command -v tmux > /dev/null && tmux && exit 0' >> /home/${SSH_USER}/.bashrc
     fi
     
     chown ${SSH_USER}:${SSH_USER} /home/${SSH_USER}
